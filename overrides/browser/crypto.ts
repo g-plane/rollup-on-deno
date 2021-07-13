fa1103d0c97ea8d73a06182685e6cfa09ae720f4
Pig Fang <g-plane@hotmail.com>
2021-07-13 09:30:00 +0800

    align behavior of "crypto" module

--- overrides/browser/crypto.ts
+++ overrides/browser/crypto.ts
@@ -1,5 +1,12 @@
 import { createHash as create } from "https://deno.land/std@0.100.0/hash/mod.ts";
+import type { Hasher } from "https://deno.land/std@0.100.0/hash/mod.ts";
 
 export function createHash() {
-  return create("sha256");
+  const hasher = create("sha256");
+
+  const update: Hasher["update"] = (data) => hasher.update(data);
+  const digest = (format?: Parameters<Hasher["toString"]>[0]): string =>
+    hasher.toString(format);
+
+  return { update, digest };
 }
